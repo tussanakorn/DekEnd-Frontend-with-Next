9e4c13a4174eb6c58bd1cedb9a4222435845b397
tussanakorn <tussanakorn2000@gmail.com>
2025-02-09 21:31:51 +0700

    feat: login

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next"
+import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar"
+
+export const metadata: Metadata = {
+  title: "Dekend - เว็บหาที่ฝึกงาน หางาน สำหรับนักศึกษา",
+  description: "เว็บไซต์สำหรับค้นหางาน ฝึกงาน สำหรับนักศึกษาและบัณฑิตจบใหม่",
+}
+
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="th">
+      <body>
+        <Navbar />
+          {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,14 @@
-import type { Metadata } from "next"
 import "./globals.css"
-import Footer from "@/components/Footer"
-import Navbar from "@/components/Navbar"
-
-export const metadata: Metadata = {
-  title: "Dekend - เว็บหาที่ฝึกงาน หางาน สำหรับนักศึกษา",
-  description: "เว็บไซต์สำหรับค้นหางาน ฝึกงาน สำหรับนักศึกษาและบัณฑิตจบใหม่",
-}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="th">
       <body>
-        <Navbar />
-          {children}
-        <Footer />
+        {children}
       </body>
     </html>
   )
